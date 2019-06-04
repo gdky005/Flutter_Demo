510a00c5c9ff2f8985c392c1a003f6982fb478c8
WangQing <wangqing11@100tal.com>
2019-06-04 16:01:26 +0800

    1. 自定义水波纹样式。

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,10 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Home(),
-      theme: ThemeData(primarySwatch: Colors.yellow),
+      theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+          splashColor: Colors.white70),
     );
   }
 }
