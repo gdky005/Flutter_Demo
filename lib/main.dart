b0e6d03dcc51ad49eb6ad71dd8167e401a8b544d
WangQing <wangqing11@100tal.com>
2019-06-04 17:20:17 +0800

    1. 抽离 侧边栏 demo 到小组件。

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'demo/darw_demo.dart';
+
 void main() => runApp(App());
 
 class App extends StatelessWidget {
@@ -54,61 +56,7 @@ class Home extends StatelessWidget {
           ),
           drawer: Container(
             color: Colors.white,
-            child: Drawer(
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: <Widget>[
-                  UserAccountsDrawerHeader(
-                    accountName: Text(
-                      "WangQing",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    accountEmail: Text("WangQing@zkteam.cc"),
-                    currentAccountPicture: CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          'https://ss0.bdstatic.com/7Ls0a8Sm1A5BphGlnYG/sys/portrait/item/fda13803.jpg'),
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.green[400],
-                      image: DecorationImage(
-                          image: NetworkImage(
-                              'http://f.hiphotos.baidu.com/image/h%3D300/sign=d985fb87d81b0ef473e89e5eedc551a1/b151f8198618367aa7f3cc7424738bd4b31ce525.jpg'),
-                          fit: BoxFit.cover,
-                          colorFilter: ColorFilter.mode(
-                              Colors.green[400].withOpacity(0.6),
-                              BlendMode.hardLight)),
-                    ),
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Messages',
-                      textAlign: TextAlign.right,
-                    ),
-                    trailing:
-                        Icon(Icons.message, color: Colors.black12, size: 22.0),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Favorite',
-                      textAlign: TextAlign.right,
-                    ),
-                    trailing:
-                        Icon(Icons.favorite, color: Colors.black12, size: 22.0),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                  ListTile(
-                    title: Text(
-                      'Settings',
-                      textAlign: TextAlign.right,
-                    ),
-                    trailing:
-                        Icon(Icons.settings, color: Colors.black12, size: 22.0),
-                    onTap: () => Navigator.pop(context),
-                  ),
-                ],
-              ),
-            ),
+            child: DrawDemo(),
           ),
         ));
   }
