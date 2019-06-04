fde3c6f52ecb91a5857df175b5f09f0cdfae2b78
WangQing <wangqing11@100tal.com>
2019-06-04 15:41:33 +0800

    1. Flutter 显示 TabBar, TabBarView, TabController，显示选项卡。

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,39 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: "navigration",
-            onPressed: () => debugPrint("菜单")),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.search),
-              tooltip: "navigration",
-              onPressed: () => debugPrint("搜索按钮")),
-        ],
-        title: Text("WangQing"),
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: null,
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            leading: IconButton(
+                icon: Icon(Icons.menu),
+                tooltip: "navigration",
+                onPressed: () => debugPrint("菜单")),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: "navigration",
+                  onPressed: () => debugPrint("搜索按钮")),
+            ],
+            title: Text("WangQing"),
+            centerTitle: true,
+            elevation: 0.0,
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+            ],
+          ),
+        ));
   }
 }
