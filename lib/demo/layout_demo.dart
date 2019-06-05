52d1d946d058c3aa1f13417f5f5fb66fd808103f
WangQing <wangqing11@100tal.com>
2019-06-05 13:31:21 +0800

    AspectRatio 设置宽高比的容器大小。

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -7,7 +7,41 @@ class LayoutDemo extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Stack(
+          AspectRatio(
+            aspectRatio: 16/9,
+            child: Container(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+            )
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IconBadge extends StatelessWidget {
+  final IconData icon;
+  final double size;
+
+  IconBadge(this.icon, {this.size = 32.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(icon, size: size, color: Colors.white),
+      width: size + 60,
+      height: size + 60,
+      color: Color.fromRGBO(3, 54, 255, 1.0),
+    );
+  }
+}
+
+
+class StackDemo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+
+    return Stack(
             alignment: Alignment.topLeft,
             children: <Widget>[
               SizedBox(
@@ -134,26 +168,6 @@ class LayoutDemo extends StatelessWidget {
                   ),
               ),
             ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class IconBadge extends StatelessWidget {
-  final IconData icon;
-  final double size;
-
-  IconBadge(this.icon, {this.size = 32.0});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Icon(icon, size: size, color: Colors.white),
-      width: size + 60,
-      height: size + 60,
-      color: Color.fromRGBO(3, 54, 255, 1.0),
-    );
+          );
   }
 }
