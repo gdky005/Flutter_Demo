5f2f2325dcc86273fb2d94ed2eb7c9bac33af916
WangQing <wangqing11@100tal.com>
2019-06-05 13:25:42 +0800

    添加一摞小部件的雪花效果。

--- lib/demo/layout_demo.dart
+++ lib/demo/layout_demo.dart
@@ -3,37 +3,138 @@ import 'package:flutter/material.dart';
 class LayoutDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-
         children: <Widget>[
-          SizedBox(
-            width: 200,
-            height: 300,
-            child: Container(
-              alignment: Alignment(0,0),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+          Stack(
+            alignment: Alignment.topLeft,
+            children: <Widget>[
+              SizedBox(
+                width: 200,
+                height: 300,
+                child: Container(
+                  alignment: Alignment(0, -0.9),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+
+                ),
               ),
-              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0,),
-            ),
-          ),
-          SizedBox(
-            height: 32,
-          ),
-          SizedBox(
-            width: 100,
-            height: 100,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(3, 54, 255, 1.0),
-                borderRadius: BorderRadius.circular(8.0),
+              SizedBox(
+                height: 32,
               ),
-              child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0,),
-            ),
-          ),
+              SizedBox(
+                width: 100,
+                height: 100,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(3, 54, 255, 1.0),
+                    // borderRadius: BorderRadius.circular(8.0),
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        Color.fromRGBO(7, 102, 255, 1),
+                        Color.fromRGBO(3, 54, 255, 1),
+
+                      ]
+                    )
+                  ),
+                  child: Icon(
+                    Icons.brightness_2,
+                    color: Colors.white,
+                    size: 32.0,
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 20,
+                top: 20,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                    size: 9.0,
+                  ),
+              ),
+              Positioned(
+                right: 50,
+                top: 90,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                    size: 10.0,
+                  ),
+              ),
+              Positioned(
+                right: 80,
+                top: 70,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                    size: 20.0,
+                  ),
+              ),
+              Positioned(
+                right: 100,
+                top: 210,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                    size: 14.0,
+                  ),
+              ),
+              Positioned(
+                right: 90,
+                top: 150,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                  ),
+              ),
+              Positioned(
+                right: 100,
+                top: 160,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+              ),
+              Positioned(
+                right: 100,
+                top: 80,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                  ),
+              ),
+              Positioned(
+                right: 10,
+                top: 120,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                  ),
+              ),
+              Positioned(
+                right: 20,
+                top: 120,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                  ),
+              ),
+              Positioned(
+                right: 50,
+                top: 220,
+                child: Icon(
+                    Icons.ac_unit,
+                    color: Colors.white,
+                  ),
+              ),
+            ],
+          )
         ],
       ),
     );
