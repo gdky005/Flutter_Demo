7203aff05b048a63a0c7fc4541588a000465f1a2
WangQing <wangqing11@100tal.com>
2019-06-06 13:08:42 +0800

    Material 的圆角没生效。

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import '../model/post.dart';
 
 class SliverDemo extends StatelessWidget {
@@ -10,10 +11,58 @@ class SliverDemo extends StatelessWidget {
           SliverSafeArea(
             sliver: SliverPadding(
               padding: EdgeInsets.all(8),
-              sliver: SliverGrideDemo(),
+              sliver: SliverListDemo(),
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class SliverListDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          return Padding(
+            padding: EdgeInsets.only(bottom: 32),
+            child: Material(
+                borderRadius: BorderRadius.circular(50),
+                elevation: 14,
+                shadowColor: Colors.grey.withOpacity(0.5),
+                child: Stack(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 16 / 9,
+                      child: Image.network(
+                        posts[index].imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Positioned(
+                      top: 32,
+                      left: 32,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            posts[index].title,
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                          Text(
+                            posts[index].author,
+                            style: TextStyle(fontSize: 13, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+          );
+        },
+        childCount: posts.length,
       ),
     );
   }
