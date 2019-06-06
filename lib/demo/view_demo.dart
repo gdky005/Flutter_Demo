19c97fd5d041b15f0e140794b3450686510ac91a
WangQing <wangqing11@100tal.com>
2019-06-06 12:02:57 +0800

    pageview 按需生成页面。

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -1,8 +1,52 @@
 import 'package:flutter/material.dart';
+import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+
+  Widget _pageItemBuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          bottom: 8.0,
+          left: 8.0,
+          child: Column(
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white)
+              ),
+              Text(
+                posts[index].author,
+                style: TextStyle(color: Colors.white)
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: posts.length,
+      itemBuilder: _pageItemBuilder,
+
+    );
+  }
+}
+
+
+class PageViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
     return PageView(
       // pageSnapping: false,
       // reverse: true,
--- lib/model/post.dart
+++ lib/model/post.dart
@@ -21,54 +21,54 @@ final List<Post> posts = [
     title: 'Test2',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://b.hiphotos.baidu.com/image/h%3D300/sign=ad628627aacc7cd9e52d32d909032104/32fa828ba61ea8d3fcd2e9ce9e0a304e241f5803.jpg',
   ),
   Post(
     title: 'Test3',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://e.hiphotos.baidu.com/image/h%3D300/sign=a9e671b9a551f3dedcb2bf64a4eff0ec/4610b912c8fcc3cef70d70409845d688d53f20f7.jpg',
   ),
   Post(
     title: 'Test4',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://e.hiphotos.baidu.com/image/h%3D300/sign=907f6e689ddda144c5096ab282b6d009/dc54564e9258d1092f7663c9db58ccbf6c814d30.jpg',
   ),
   Post(
     title: 'Test5',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://c.hiphotos.baidu.com/image/h%3D300/sign=0cc74ef9a3773912db268361c8188675/9922720e0cf3d7ca810f3732f81fbe096a63a9fd.jpg',
   ),
   Post(
     title: 'Test6',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://g.hiphotos.baidu.com/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg',
   ),
   Post(
     title: 'Test7',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://a.hiphotos.baidu.com/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg',
   ),
   Post(
     title: 'Test8',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://b.hiphotos.baidu.com/image/h%3D300/sign=92afee66fd36afc3110c39658318eb85/908fa0ec08fa513db777cf78376d55fbb3fbd9b3.jpg',
   ),
   Post(
     title: 'Test9',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://f.hiphotos.baidu.com/image/h%3D300/sign=0c78105b888ba61ec0eece2f713597cc/0e2442a7d933c8956c0e8eeadb1373f08202002a.jpg',
   ),
   Post(
     title: 'Test10',
     author: 'WangQing1',
     imageUrl:
-        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559307530720&di=47d93c93e269bc14ab64b0b957ca6c55&imgtype=0&src=http%3A%2F%2Fs3.sinaimg.cn%2Fmw690%2F0019wuMRty6X7eKstWi12%26690',
+        'http://b.hiphotos.baidu.com/image/h%3D300/sign=05b297ad39fa828bce239be3cd1e41cd/0eb30f2442a7d9337119f7dba74bd11372f001e0.jpg',
   ),
 ];
